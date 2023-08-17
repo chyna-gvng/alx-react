05df38a80adcc9e90090acdd6f3494fb2d11ad2d
Walter <w.angoye@alustudent.com>
2023-08-17 10:05:26 +0300

    Updated 8-main.js

--- 0x06-React_Immutable/8-main.js
+++ 0x06-React_Immutable/8-main.js
@@ -1,21 +1,21 @@
-import { Map } from 'immutable';
-import printBestStudents from './8-seq';
+import { Map } from "immutable";
+import printBestStudents from "./8-seq";
 
 const obj = {
   1: {
     score: 99,
-    firstName: 'guillaume',
-    lastName: 'salva',
+    firstName: "guillaume",
+    lastName: "salva",
   },
   2: {
     score: 70,
-    firstName: 'Julien',
-    lastName: 'Barbier',
+    firstName: "Julien",
+    lastName: "Barbier",
   },
   3: {
     score: 69,
-    firstName: 'Sylvain',
-    lastName: 'Kalache',
+    firstName: "Sylvain",
+    lastName: "Kalache",
   },
 };
 
