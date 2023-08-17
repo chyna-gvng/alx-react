3f420eeb52ea2afead363c12ac4e50a8b6240b8f
Walter <w.angoye@alustudent.com>
2023-08-17 10:06:35 +0300

    Deleted 3-main.js

--- 0x06-React_Immutable/3-main.js
+++
@@ -1,5 +0,0 @@
-import { getListObject, addElementToList } from "./3-list";
-
-console.log(getListObject(["one", "two", "three"]));
-
-console.log(addElementToList(["one", "two", "three"], "four"));
