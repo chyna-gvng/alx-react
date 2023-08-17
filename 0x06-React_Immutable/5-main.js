6f7a48e7695e28692af9f181713da679bb67b871
Walter <w.angoye@alustudent.com>
2023-08-17 10:05:09 +0300

    Updated 5-main.js

--- 0x06-React_Immutable/5-main.js
+++ 0x06-React_Immutable/5-main.js
@@ -1,7 +1,7 @@
-import { concatElements, mergeElements } from './5-merge';
+import { concatElements, mergeElements } from "./5-merge";
 
-const arr1 = ['one', 'two'];
-const arr2 = ['three', 'four'];
+const arr1 = ["one", "two"];
+const arr2 = ["three", "four"];
 
 const obj1 = {
   fear: true,
@@ -10,12 +10,12 @@ const obj1 = {
   thing: -914767132,
 };
 const obj2 = {
-  1: 'Liam',
-  2: 'Noah',
-  3: 'Elijah',
-  4: 'Oliver',
-  5: 'Jacob',
-  6: 'Lucas',
+  1: "Liam",
+  2: "Noah",
+  3: "Elijah",
+  4: "Oliver",
+  5: "Jacob",
+  6: "Lucas",
 };
 
 console.log(concatElements(arr1, arr2), concatElements(arr1, arr2).toJS());
